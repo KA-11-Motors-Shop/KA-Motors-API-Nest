46bcd1ef6e26e869cbc371e9c159b28322255bd4
Fábio <fabioalmeida0896@gmail.com>
2022-08-16 13:57:36 -0300

    test: e2e anuncio tests and test config

---
+++ test/anuncios.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('anuncios (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/anuncios (GET)', () => {
+    return request(app.getHttpServer())
+      .get('/anuncios')
+      .expect(200)
+      
+  });
+});
