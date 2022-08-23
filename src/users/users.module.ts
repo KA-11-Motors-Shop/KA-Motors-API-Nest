a8adab6378072b6513dcbc1fcd46ad3636a1421b
Fábio <fabioalmeida0896@gmail.com>
2022-08-23 13:54:10 -0300

    refactor: refactored the logic to comply with new anuncios code

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,13 +1,18 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Endereco } from '../enderecos/entities/endereco.entity';
 import { AuthService } from './auth/auth.service';
+import { CurrentUserMiddleware } from './middlewares/current-user.middleware';
 @Module({
   imports: [TypeOrmModule.forFeature([User, Endereco])],
   controllers: [UsersController],
   providers: [UsersService, AuthService],
 })
-export class UsersModule {}
+export class UsersModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CurrentUserMiddleware).forRoutes('*')
+  }
+}
