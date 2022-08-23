3b592e4e38e94ccf5a4849be1432d1f18b73becf
Fábio <fabioalmeida0896@gmail.com>
2022-08-23 13:52:59 -0300

    feat: created user middleware

---
+++ src/users/middlewares/current-user.middleware.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+import { NestMiddleware, Injectable } from '@nestjs/common';
+import { UsersService } from '../users.service';
+import { User } from '../entities/user.entity';
+
+declare global {
+  namespace Express {
+    interface Request {
+      currentUser?: User;
+    }
+  }
+}
+
+@Injectable()
+export class CurrentUserMiddleware implements NestMiddleware {
+  constructor(private userService: UsersService) {}
+
+  async use(req: Request, res: Response, next: NextFunction) {
+    const userId = req.session.userId || {};
+    console.log(req.session)
+
+    if (userId) {
+      const user = await this.userService.findOne(userId);
+      req.currentUser = user;
+    }
+
+    next();
+  }
+}
